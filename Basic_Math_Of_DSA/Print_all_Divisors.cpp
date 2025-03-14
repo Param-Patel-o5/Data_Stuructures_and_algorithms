9cc6ffe750973e8139d541305da7dfb92e0b79f3
Param-Patel-o5 <parampatel.005@gmail.com>
2025-03-14 10:50:08 +0530

    Print_all_Divisors.cpp
    Given a positive integer n, The task is to find the value of Σi F(i) where i is from 1 to n and function F(i) is defined as the sum of all divisors of i.

---
+++ Basic_Math_Of_DSA/Print_all_Divisors.cpp
@@ -0,0 +1,23 @@
+// User function Template for C++
+class Solution
+{
+public:
+    int sumOfDivisors(int n)
+    {
+        // Write Your Code here
+     
+        int sum = 0;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                if (i % j == 0)
+                {
+                    sum = sum + j;
+                }
+            }
+        }
+        return sum;
+    }
+    
+    };
