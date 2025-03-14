f7a5194de640239ab6082d9d36a59facd4c0ea5e
Param-Patel-o5 <parampatel.005@gmail.com>
2025-03-14 10:47:09 +0530

    Create Palindrome.cpp
    Given an integer x, return true if x is a palindrome, and false otherwise.

---
+++ Basic_Math_Of_DSA/Palindrome.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        int last_digit;
+        long int reversed = 0;
+        int n = x;
+        if (x < 0)
+        {
+            return false;
+        }
+        else if (x >= 0)
+        {
+            while (n != 0)
+            {
+                last_digit = n % 10;
+                reversed = reversed * 10 + last_digit;
+                n = n / 10;
+            }
+            if (reversed == x)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return 0;
+    }
+};
